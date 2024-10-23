d7a5f717f844a7db8eba0966d057778868f37686
amellagu <amellagu>
2024-10-23 15:42:47 +0100

    ok

--- groupie-tracker/main.go
+++ groupie-tracker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json" //////////
 	"fmt"
 	"html/template"
 	"log"
@@ -8,15 +9,21 @@ import (
 )
 
 type Data struct {
-	Tit   string
-	Hepe  string
-	Forme string
-	Bode  string
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members    []string   `json:"members"`
+	CreationDate int   `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Felations    string   `json:"relations"`
 }
 
 func main() {
+	
 	http.HandleFunc("/home", handler)
-
+	
 	log.Println("Server start in : http://localhost:3000/home")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
@@ -25,14 +32,32 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World !!")
+
+	// api url
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+	
+	// http get request
+	getResp, errG := http.Get(url)
+	if errG != nil {
+		log.Fatal("Error: http get request")
+	}
+	defer getResp.Body.Close()
+	
+	// check status is OK
+	if getResp.StatusCode != 200 {
+		log.Fatal("Error: statu code is not 200", getResp.StatusCode)
+	}
+	
+	// decode the JSON response into a stract
+	var apiRes []Data
+	errj := json.NewDecoder(getResp.Body).Decode(&apiRes)
+	if errj != nil {
+		log.Fatalf("Error: json %v", errj)
+	}
+	fmt.Println(apiRes)
 
 	tmpl, _ := template.ParseFiles("template.html")
-	data := Data{
-		Tit:   "title",
-		Hepe:  "heading",
-		Forme: "formulaire",
-		Bode:  "bodying",
-	}
-	tmpl.Execute(w, data)
+	data1 := apiRes[0]
+	fmt.Println(data1)
+	tmpl.Execute(w, data1)
 }
