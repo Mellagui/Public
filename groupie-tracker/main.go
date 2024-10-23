a0fd747cf6927152f49535f2141ff90b9a26e017
amellagu <amellagu>
2024-10-23 10:17:24 +0100

    ok

--- groupie-tracker/main.go
+++ groupie-tracker/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 type Data struct {
+	Tit   string
+	Hepe  string
+	Forme string
+	Bode  string
 }
 
 func main() {
@@ -22,6 +27,12 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World !!")
 
-	//tmpl, _ := template.ParseFiles("template.html")
-
+	tmpl, _ := template.ParseFiles("template.html")
+	data := Data{
+		Tit:   "title",
+		Hepe:  "heading",
+		Forme: "formulaire",
+		Bode:  "bodying",
+	}
+	tmpl.Execute(w, data)
 }
